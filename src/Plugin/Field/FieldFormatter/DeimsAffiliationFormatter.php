6788f1e9674b951eb2d722f321b7cc32cb6743da
stopopol <christoph.wohner@umweltbundesamt.at>
2019-07-24 14:10:49 +0200

    Add files via upload

---
+++ src/Plugin/Field/FieldFormatter/DeimsAffiliationFormatter.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\deims_affiliation_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'DeimsAffiliationFormatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "deims_affiliation_formatter",
+ *   label = @Translation("DEIMS Affiliation Formatter"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   },
+ *   quickedit = {
+ *     "editor" = "disabled"
+ *   }
+ * )
+ */
+ 
+class DeimsAffiliationFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+   
+ 
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Formats the affiliation field of Drupal.');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+	
+	// Render each element as markup in case of multi-values.
+
+	foreach ($items as $delta => $item) {
+		  
+	  // Desired fields: field_network_name, field_network_specific_site_code, field_network_verified;
+	  if ($item->entity->field_network_name->target_id) {
+		$network_name_tid = $item->entity->field_network_name->target_id;
+	    $full_taxonomy_term_object = taxonomy_term_load($network_name_tid);
+		$network_label = $full_taxonomy_term_object->getName();	  
+	  }
+	  else {
+		break;
+	  }  
+	  // add network related site code if existant
+	  if ($item->entity->field_network_specific_site_code->value) {
+		$network_site_code = " (" . $item->entity->field_network_specific_site_code->value . ")";
+	  }
+	  else {
+		$network_site_code = "";
+	  }
+	  // add verification status, if it is a verified site
+	  if ($item->entity->field_network_verified->value) {
+		$network_site_verified = $item->entity->field_network_verified->value;
+		$network_site_verified = '<sup class="green-check">✔</sup>';
+		$network_element = '<div class="verification-tooltip">' . $network_label . $network_site_verified . $network_site_code . '<span class="verification-tooltiptext">This site is a verified ' .$network_label. ' member</span></div>';
+	  
+	  }
+	  else {
+		  $network_site_verified = "";
+		  $network_element = '<div class="verification-tooltip">' . $network_label . $network_site_verified . $network_site_code . '</div>';
+	  }
+	  
+	  
+	  $element[$delta] = [
+		'#markup' => $network_element,
+		'#attached' => [
+			'library' => [
+			  'deims_affiliation_formatter/deims-affiliation-formatter-style',
+			],
+		]
+	  ];
+	} 
+	// sort elements of result alphabetically
+	sort($element);
+	return $element;
+  }
+	
+}
---
+++ src/Plugin/Field/FieldFormatter/DeimsAffiliationJSONFormatter.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Drupal\deims_affiliation_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/**
+ * Plugin implementation of the 'DeimsAffiliationJSONFormatter' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "deims_affiliation_json_formatter",
+ *   label = @Translation("Deims Affiliation JSON Formatter"),
+ *   field_types = {
+ *     "entity_reference_revisions"
+ *   },
+ *   quickedit = {
+ *     "editor" = "disabled"
+ *   }
+ * )
+ */
+ 
+class DeimsAffiliationJSONFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+   
+ 
+  public function settingsSummary() {
+    $summary = [];
+    $summary[] = $this->t('Formats the affiliation field of Drupal as a JSON.');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+	
+	// Render each element as markup in case of multi-values.
+	$pre_json_array = [];
+	foreach ($items as $delta => $item) {
+	  $temp_array = [];
+	  // Desired fields: field_network_name, field_network_specific_site_code, field_network_verified;
+	  if ($item->entity->field_network_name->target_id) {
+		$network_name_tid = $item->entity->field_network_name->target_id;
+	    $full_taxonomy_term_object = taxonomy_term_load($network_name_tid);
+		$temp_array['name'] = $full_taxonomy_term_object->getName();
+	  }
+	  else {
+		break;
+	  }  
+
+	  if ($item->entity->field_network_specific_site_code->value) {
+		$temp_array['code'] = $item->entity->field_network_specific_site_code->value;
+	  }
+	  else {
+		$temp_array['code'] = "";
+	  }
+	  
+	  if ($item->entity->field_network_verified->value) {
+		$temp_array['verified'] = true;
+	  }
+	  else {
+		$temp_array['verified'] = false;
+	  }
+	
+	  array_push($pre_json_array, $temp_array);
+	  
+	}
+
+	$json_object = json_encode($pre_json_array);
+	$json_element = substr($json_object, 1, -1);
+	// print json_encoded array into a single-value element output
+	$element[$delta] = [
+		'#markup' => $json_element
+	];
+	
+	return $element;
+  }
+	
+}
