0907345cb770c3dc403b0759ad833f621f7f19d4
stopopol <christoph.wohner@umweltbundesamt.at>
2021-05-05 15:56:45 +0200

    Update DeimsAffiliationFormatter.php

--- src/Plugin/Field/FieldFormatter/DeimsAffiliationFormatter.php
+++ src/Plugin/Field/FieldFormatter/DeimsAffiliationFormatter.php
@@ -59,7 +59,11 @@ class DeimsAffiliationFormatter extends FormatterBase {
 	  }  
 	  // add network related site code if existant
 	  if ($item->entity->field_network_specific_site_code->value) {
-		$network_site_code = "<sub>  (" . $item->entity->field_network_specific_site_code->value . ")</sub>";
+		$network_site_code = $item->entity->field_network_specific_site_code->value;
+		  if (filter_var($network_site_code, FILTER_VALIDATE_URL) === TRUE) {
+			  $network_site_code = ' <a href="' . $network_site_code . '">' . $network_site_code . "</a>";
+		}
+		$network_site_code = "<sub>  (" . $network_site_code . ")</sub>";
 	  }
 	  else {
 		$network_site_code = "";
